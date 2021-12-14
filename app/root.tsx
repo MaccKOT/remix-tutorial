3de3300a9e3467e4fb9b4f045865ef5aecdbd22e
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2021-12-14 11:49:18 +0400

    first commit

---
+++ app/root.tsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <html>
+      <head>
+        <meta charSet='utf-8' />
+        <title>Remix Jokes App</title>
+      </head>
+      <body>
+        <h1>Hello World</h1>
+      </body>
+    </html>
+  );
+}

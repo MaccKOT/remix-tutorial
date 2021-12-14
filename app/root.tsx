1d482ba5f34d795dbc3749b008412ecbb0551e4b
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2021-12-14 12:58:17 +0400

    Add styling and html in components

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,28 @@
 import { LiveReload } from 'remix'; // component for HMR
 import { Outlet } from 'remix'; // component for routes usage
+import { Links } from 'remix'; // links component (.css)
+import type { LinksFunction } from 'remix'; // for global css
+
+import globalStylesUrl from './styles/global.css';
+import globalMediumStylesUrl from './styles/global-medium.css';
+import globalLargeStylesUrl from './styles/global-large.css';
+
+export const links: LinksFunction = () => [
+  {
+    rel: 'stylesheet',
+    href: globalStylesUrl,
+  },
+  {
+    rel: 'stylesheet',
+    href: globalMediumStylesUrl,
+    media: 'print, (min-width: 640px)',
+  },
+  {
+    rel: 'stylesheet',
+    href: globalLargeStylesUrl,
+    media: 'screen and (min-width: 1024px)',
+  },
+];
 
 export default function App() {
   return (
@@ -7,9 +30,9 @@ export default function App() {
       <head>
         <meta charSet='utf-8' />
         <title>Remix Jokes App</title>
+        <Links />
       </head>
       <body>
-        <h1>Hello World!</h1>
         <Outlet />
         {process.env.NODE_ENV === 'development' ? <LiveReload /> : null}
       </body>
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,31 @@
 // root path /
+import { Link } from 'remix';
+import { LinksFunction } from 'remix';
+import stylesUrl from '../styles/index.css';
+
+// rendering <link href="/styles/index.css" rel="stylesheet">
+export const links: LinksFunction = () => [
+  {
+    rel: 'stylesheet',
+    href: stylesUrl,
+  },
+];
 
 export default function IndexRoute() {
-  return <div>Hello index route</div>;
+  return (
+    <div className='container'>
+      <div className='content'>
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to='jokes'>Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
